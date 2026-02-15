3db65c94a7fb0aae24b1a750662ed295334ae7bb
Syaa_ke <enderman0005678@gmail.com>
2026-02-15 15:35:57 +0900

    ピンクガーネット系のアイテムを追加

--- src/main/java/com/syake/tutorialmod/TutorialMod.java
+++ src/main/java/com/syake/tutorialmod/TutorialMod.java
@@ -1,5 +1,8 @@
 package com.syake.tutorialmod;
 
+import com.syake.tutorialmod.block.ModBlocks;
+import com.syake.tutorialmod.item.ModItemGroups;
+import com.syake.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
